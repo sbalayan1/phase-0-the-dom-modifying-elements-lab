f824c3e8e6c7b6c089c16043fd2dc79fcf8633d4
sbalayan1 <balayans2014@yahoo.com>
2021-04-30 15:12:57 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+//removes the node main#main
+main.remove();
+
+
+//create variable newHeader and new h1 element 
+const newHeader = document.createElement('h1');
+
+//set newHeader attribute to victory 
+newHeader.setAttribute("id",'victory');
+
+//grab newHeader variable and set innerHTML attribute to message
+newHeader.innerHTML = "Sean is the champion";
+
+
+
